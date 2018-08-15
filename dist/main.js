dengun-blockchain/frasesdorante

Set initial project

dist/main.js
@@ -1,0 +1,1 @@
+!function(t){var e={};function n(r){if(e[r])return e[r].exports;var o=e[r]={i:r,l:!1,exports:{}};return t[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=t,n.c=e,n.d=function(t,e,r){n.o(t,e)||Object.defineProperty(t,e,{enumerable:!0,get:r})},n.r=function(t){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(t,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(t,"__esModule",{value:!0})},n.t=function(t,e){if(1&e&&(t=n(t)),8&e)return t;if(4&e&&"object"==typeof t&&t&&t.__esModule)return t;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:t}),2&e&&"string"!=typeof t)for(var o in t)n.d(r,o,function(e){return t[e]}.bind(null,o));return r},n.n=function(t){var e=t&&t.__esModule?function(){return t.default}:function(){return t};return n.d(e,"a",e),e},n.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},n.p="",n(n.s=0)}([function(t,e,n){"use strict";var r=a(n(1)),o=a(n(2));function a(t){return t&&t.__esModule?t:{default:t}}var u=new r.default(o.default),i=window.quote,l=window.twitteranchor;window.quotebutton.onclick=function(){var t=u.getRandomQuoteOrDialog(),e=encodeURIComponent(t.join(" - "));e.length>125&&(e=e.substr(0,125)+"..."),i.innerHTML=t.reduce(function(t,e){return"\n      "+t+"\n      <p>"+e+"</p>\n    "},""),l.href="https://twitter.com/intent/tweet?via=Mi_PiCo&hashtags=frasesdorante&url=https://painatalman.github.io/frasesdorante&text="+e},window.quotebutton.click()},function(t,e,n){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var r=function(){function t(t,e){for(var n=0;n<e.length;n++){var r=e[n];r.enumerable=r.enumerable||!1,r.configurable=!0,"value"in r&&(r.writable=!0),Object.defineProperty(t,r.key,r)}}return function(e,n,r){return n&&t(e.prototype,n),r&&t(e,r),e}}();function o(t){if(Array.isArray(t)){for(var e=0,n=Array(t.length);e<t.length;e++)n[e]=t[e];return n}return Array.from(t)}var a=function(){function t(e){!function(t,e){if(!(t instanceof e))throw new TypeError("Cannot call a class as a function")}(this,t),this.quotesAndDialogs=[].concat(o(e.quotes),o(e.dialogs)),this.extras=e.extras,this.lastIndex=null}return r(t,[{key:"getRandomQuoteOrDialog",value:function(){var t=this,e=0,n=this.quotesAndDialogs.length;if(n>1)do{e=Math.floor(Math.random()*n)}while(e===this.lastIndex);this.lastIndex=e;var r=this.quotesAndDialogs[e];return r instanceof Array||(r=[r]),r.map(function(e){return e.replace(/\$(\w+)/g,function(e,n){return t.getRandomExtra(n)})})}},{key:"getRandomExtra",value:function(t){if(!this.extras[t])throw new Error("there are no extras of type"+t);return this.extras[t][Math.floor(Math.random()*this.extras[t].length)]}}]),t}();a.extraTypeWildcard="$",e.default=a},function(t){t.exports={quotes:["Faz um ICO!","$techs é o futuro!"],dialogs:[["E como está o departamento de Blockchain?","Qual departamento?"]],extras:{techs:["blockchain","ICO"]}}}]);

Painatalman/yamcha-facts

dist/main.js
@@ -1,0 +1,1 @@
+!function(a){var e={};function t(n){if(e[n])return e[n].exports;var o=e[n]={i:n,l:!1,exports:{}};return a[n].call(o.exports,o,o.exports,t),o.l=!0,o.exports}t.m=a,t.c=e,t.d=function(a,e,n){t.o(a,e)||Object.defineProperty(a,e,{enumerable:!0,get:n})},t.r=function(a){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(a,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(a,"__esModule",{value:!0})},t.t=function(a,e){if(1&e&&(a=t(a)),8&e)return a;if(4&e&&"object"==typeof a&&a&&a.__esModule)return a;var n=Object.create(null);if(t.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:a}),2&e&&"string"!=typeof a)for(var o in a)t.d(n,o,function(e){return a[e]}.bind(null,o));return n},t.n=function(a){var e=a&&a.__esModule?function(){return a.default}:function(){return a};return t.d(e,"a",e),e},t.o=function(a,e){return Object.prototype.hasOwnProperty.call(a,e)},t.p="",t(t.s=0)}([function(a,e,t){"use strict";var n=r(t(1)),o=r(t(2));function r(a){return a&&a.__esModule?a:{default:a}}var i=new Audio("https://www.myinstants.com/media/sounds/dbz-teleport.mp3"),s=new n.default(o.default),c=window.quote,h=window.twitteranchor;function l(){c.classList.remove("zoomIn"),c.classList.add("zoomOut")}function u(){c.classList.remove("zoomOut"),c.classList.add("zoomIn")}function d(){var a=s.getRandomQuoteOrDialog(),e=encodeURIComponent(a.join(" - "));e.length>125&&(e=e.substr(0,125)+"..."),c.innerHTML=a.reduce(function(a,e){return"\n      "+a+"\n      <p>"+e+"</p>\n    "},""),h.href="https://twitter.com/intent/tweet?via=Mi_PiCo&hashtags=frasesdorante&url=https://painatalman.github.io/frasesdorante&text="+e}function m(){d(),u(),c.removeEventListener("animationend",m)}window.quotebutton.onclick=function(){i.play(),c.addEventListener("animationend",m),l()},l(),d(),u()},function(a,e,t){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var n=function(){function a(a,e){for(var t=0;t<e.length;t++){var n=e[t];n.enumerable=n.enumerable||!1,n.configurable=!0,"value"in n&&(n.writable=!0),Object.defineProperty(a,n.key,n)}}return function(e,t,n){return t&&a(e.prototype,t),n&&a(e,n),e}}();function o(a){if(Array.isArray(a)){for(var e=0,t=Array(a.length);e<a.length;e++)t[e]=a[e];return t}return Array.from(a)}var r=function(){function a(e){!function(a,e){if(!(a instanceof e))throw new TypeError("Cannot call a class as a function")}(this,a),this.quotesAndDialogs=[].concat(o(e.quotes),o(e.dialogs)),this.extras=e.extras,this.lastIndex=null}return n(a,[{key:"getRandomQuoteOrDialog",value:function(){var a=this,e=0,t=this.quotesAndDialogs.length;if(t>1)do{e=Math.floor(Math.random()*t)}while(e===this.lastIndex);this.lastIndex=e;var n=this.quotesAndDialogs[e];return n instanceof Array||(n=[n]),n.map(function(e){return e.replace(/\$(\w+)/g,function(e,t){return a.getRandomExtra(t)})})}},{key:"getRandomExtra",value:function(a){if(!this.extras[a])throw new Error("there are no extras of type"+a);return this.extras[a][Math.floor(Math.random()*this.extras[a].length)]}}]),a}();r.extraTypeWildcard="$",e.default=r},function(a){a.exports={quotes:["A dinosaur can't teach Yamcha how to ride a ball","A lion can eat Yamcha and still be hungry","A man once said: 'If you ain't first, you're Yamcha!'","An old lady helped Yamcha cross the street","Any person who finishes last still gets ahead of Yamcha","Braille can speak Yamcha","By singing, Yamcha can be shattered by glass","George Lucas followed Yamcha's advice. Then the prequels were made","God doesn't believe in Yamcha","If Yamcha gets robbed, he has to go to jail","If you commit a foul against Yamcha, the opposing team can score a penalty","In Ultra Instinct, Yamcha can defeat TWO Saibamen","Jobbers usually perform their finishing move on Yamcha. Six times","Life's got a restraining order on Yamcha","Murphy's Law was originally planned to be Yamcha's Law, but Yamcha lost a vote by a count of zero","Toriyama forgot about Lunch because he thought it was Yamcha","Trash has to take out Yamcha once a week","When Yamcha goes for a swim, he gets burned","When Yamcha prays to god, it's always the devil that answers","Whenever Yamcha buys Kinder Surprise, it comes with no toy","Yamcha installed Chrome so that he could download Internet Explorer","Yamcha bought WinRAR","Yamcha can be opened by a jar of pickles","Yamcha can be read by books","Yamcha can be shot with a gun with no bullets","Yamcha can be stepped on by a LEGO piece","Yamcha can count to zero. Twice",'Yamcha can die from "just a flesh wound"',"Yamcha can die instantly from having part of his name written on Death Note","Yamcha can fall from the ground floor and break a leg","Yamcha can get a flat tire on a boat","Yamcha can get aids from jerking off","Yamcha can get dehydrated by drinking water","Yamcha can get viruses on a Mac","Yamcha can have sex and remain a virgin","Yamcha can lose a game by playing rock against scissors","Yamcha can own a mansion and remain homeless","Yamcha can take damage from a Magikarp's Splash attack","Yamcha can win the lottery and still owe money for the ticket","Yamcha can't afford free stuff","Yamcha can't believe in the heart of the cards","Yamcha couldn't get a role in The Room due to his bad acting","Yamcha finished a time trial once. His time was infinity","Yamcha gets booed by Roman Reigns","Yamcha gets sunburned at night","Yamcha got a speeding ticket for standing still","Yamcha got fired from being unemployed","Yamcha likes pineapple on a pizza","Yamcha lost a game of connect-1 even though he played first","Yamcha lost his virginity after his son","Yamcha retired at the age of 5","Yamcha stopped walking at the age of 1","Yamcha thinks wrestling is real","Yamcha was found guilty of his own murder","Yamcha was the only candidate for presidency. He lost","Yamcha's favourite software is Windows Vista","Yamcha's favourite Star Wars character is Jar Jar Binks"],dialogs:[],extras:{}}}]);