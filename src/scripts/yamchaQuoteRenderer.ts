caefc6d022cfc4778b528047bef616c2c91e157c
admin <admin@admins-MBP.home>
2020-02-01 01:57:41 +0000

    fix nonchanging text with reduced motion

--- src/scripts/yamchaQuoteRenderer.ts
+++ src/scripts/yamchaQuoteRenderer.ts
@@ -1,79 +1,77 @@
-import QuoteDTO from './interfaces/QuoteDTO'
-import QuoteRenderer from './interfaces/QuoteRenderer'
+import QuoteDTO from "./interfaces/QuoteDTO";
+import QuoteRenderer from "./interfaces/QuoteRenderer";
 
 class YamchaQuoteRenderer implements QuoteRenderer {
   // classnames from animatecss
   private static EFFECTS = {
-    ZOOM_IN: 'zoomIn',
-    ZOOM_OUT: 'zoomOut'
-  } 
-  private _el:HTMLElement
-  private _isFirstTime: boolean
+    ZOOM_IN: "zoomIn",
+    ZOOM_OUT: "zoomOut"
+  };
+  private _el: HTMLElement;
+  private _isFirstTime: boolean;
 
-  constructor(el:HTMLElement) {
-    this._el = el
-    this._isFirstTime = true
+  constructor(el: HTMLElement) {
+    this._el = el;
+    this._isFirstTime = true;
   }
 
   /**
    * Useful to handle animation
    */
 
-  _getPromiseForSingleAnimation(func:() => void):Promise<void> {
-    return new Promise(resolve => {
-      this._el.addEventListener(
-        'animationend',
-        () => resolve(),
-        {
-          once: true
-        }
-      )
+  _getPromiseForSingleAnimation(func: () => void): Promise<void> {
+    const hasReducedMotion = window.matchMedia(
+      "(prefers-reduced-motion: reduce)"
+    );
 
-      func()
-    })
+    if (hasReducedMotion) return Promise.resolve(func());
+
+    return new Promise(resolve => {
+      this._el.addEventListener("animationend", () => resolve(), {
+        once: true
+      });
+
+      func();
+    });
   }
 
   _hideQuote() {
-    const { ZOOM_IN, ZOOM_OUT } = YamchaQuoteRenderer.EFFECTS
-    const { _el } = this
+    const { ZOOM_IN, ZOOM_OUT } = YamchaQuoteRenderer.EFFECTS;
+    const { _el } = this;
     const setClasses = () => {
-      _el.classList.remove(ZOOM_IN)
-      _el.classList.add(ZOOM_OUT)
-    }
+      _el.classList.remove(ZOOM_IN);
+      _el.classList.add(ZOOM_OUT);
+    };
     if (this._isFirstTime) {
       return new Promise(resolve => {
-        setClasses()
-        resolve()
-      })
+        setClasses();
+        resolve();
+      });
     }
 
-    return this._getPromiseForSingleAnimation(
-      setClasses
-    )
+    return this._getPromiseForSingleAnimation(setClasses);
   }
   _showQuote() {
-    const { ZOOM_IN, ZOOM_OUT } = YamchaQuoteRenderer.EFFECTS
-    const { _el } = this
+    const { ZOOM_IN, ZOOM_OUT } = YamchaQuoteRenderer.EFFECTS;
+    const { _el } = this;
 
-    return this._getPromiseForSingleAnimation(
-      () => {
-        _el.classList.remove(ZOOM_OUT)
-        _el.classList.add(ZOOM_IN)
-      }
-    )
+    return this._getPromiseForSingleAnimation(() => {
+      _el.classList.remove(ZOOM_OUT);
+      _el.classList.add(ZOOM_IN);
+    });
   }
   _getTemplate(quote: QuoteDTO): string {
-    return `<p>${quote.getContent()}</p>`
+    return `<p>${quote.getContent()}</p>`;
   }
-  
-  async render(quote:QuoteDTO) {
-    const quoteContent = this._getTemplate(quote)
 
-    await this._hideQuote()
-    this._el.innerHTML = quoteContent
-    this._isFirstTime = false
-    return await this._showQuote()
+  async render(quote: QuoteDTO) {
+    const quoteContent = this._getTemplate(quote);
+
+    await this._hideQuote();
+    this._el.innerHTML = quoteContent;
+    this._isFirstTime = false;
+    return await this._showQuote();
   }
 }
 
-export default YamchaQuoteRenderer
+export default YamchaQuoteRenderer;
